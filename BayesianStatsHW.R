00254f371c6569e0f82e71acf5f963ef42510f5d
julianna-cola <155928379+julianna-cola@users.noreply.github.com>
2024-03-17 23:58:49 -0400

    Add files via upload

---
+++ BayesianStatsHW.R
@@ -0,0 +1,20 @@
+# Bayesian Statistics Assignment 
+# 15 March 2024
+
+# Goals: 
+#   Install JAGS and either rjags or R2jags
+#   Use jags to fit a Bayesian model to your data, in some way that at least roughly makes sense. 
+#   Discuss your prior assumptions, and compare your simple fit to an analogous frequentist fit. 
+
+# Loading in packages
+library(coda) # required for rjags 
+library(rjags)
+
+# Loading in data
+load("EUST_Clean.rda")
+
+# Priors
+
+# Fitting a Bayesian model
+
+# Comparing to an analogous frequentist fit
